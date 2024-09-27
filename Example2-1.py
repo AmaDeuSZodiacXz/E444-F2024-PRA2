815afd3320b713e78c02e829bb2c096fd5303299
Pakawat Phasook <skb@Pakawats-MacBook-Air.local>
2024-09-27 16:19:21 -0400

    example 2-1

--- Example2-1.py
+++ Example2-1.py
@@ -3,11 +3,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '<h1>Hello,Dave!</h1>'
+    return '<h1>Hello, World!</h1>'
 
 @app.route('/user/<name>')
 def user(name):
     return f'<h1>Hello, {name}!</h1>'
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return '<h1>Page Not Found</h1>', 404
 
 if __name__ == '__main__':
     app.run(debug=True)
